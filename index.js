08e6788a067cc64a25940c51c5c1379da656bdf6
Vikas Pogu <vikaspoguadf@gmail.com>
2024-03-27 10:19:02 -0400

    test app

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+
+
+// Constants
+const PORT = process.env.PORT || "8080";
+const HOST = process.env.HOST || "0.0.0.0";
+const VERSION = process.env.VERSION || "0.0";
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello RBC !!!!!!!  VERSION ${VERSION} ');
+});
+
+app.listen(PORT, HOST);
+console.log('Running on http://${HOST}:${PORT}');
